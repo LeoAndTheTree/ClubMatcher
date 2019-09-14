2ab38acf263fa6a8df71e7a50296a1488dac00c5
Yunsong Liu <yunsongliu@Yunsongs-MBP.wv.cc.cmu.edu>
2019-09-14 08:52:35 -0400

    d

--- app/src/main/java/com/example/clubmaker/Student.java
+++ app/src/main/java/com/example/clubmaker/Student.java
@@ -7,4 +7,28 @@ public class Student {
     ArrayList<int[]> classes;
     String[] preferrence;
     int clubsize = 0;
+
+    public void setTimeCommitment(int timeCommitment) {
+        this.timeCommitment = timeCommitment;
+    }
+
+    public int getTimeCommitment() {
+        return timeCommitment;
+    }
+
+    public void setClasses(ArrayList<int[]> classes) {
+        this.classes = classes;
+    }
+
+    public ArrayList<int[]> getClasses() {
+        return classes;
+    }
+
+    public void setClubsize(int clubsize) {
+        this.clubsize = clubsize;
+    }
+
+    public int getClubsize() {
+        return clubsize;
+    }
 }
