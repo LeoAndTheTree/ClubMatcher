c5e5590d126b1637798a8ee1ea5a7d70a15a26ef
Yunsong Liu <yunsongliu@Yunsongs-MBP.wv.cc.cmu.edu>
2019-09-14 09:22:42 -0400

    d

--- app/src/main/java/com/example/clubmaker/Student.java
+++ app/src/main/java/com/example/clubmaker/Student.java
@@ -5,39 +5,60 @@ import java.util.ArrayList;
 /*
 * public void setTimeCommitment(int timeCommitment);
 * public int getTimeCommitment();
-* public void setClasses(ArrayList<int[]> classes);
-* public ArrayList<int[]> getClasses();
+* public void setSchedule(ArrayList<int[]> classes);
+* public ArrayList<int[]> getSchedule();
 * public void setClubsize (int clubsize);
 * public int getClubsize (clubsize);
+* public void setTypeOfClub(String[]);
+* public String[] getTypeOfClub();
+* public boolean hasTimeCommitment();
+* public boolean hasSchedule();
+* public boolean hasTypeOfClub();
+* public boolean hasClubsize();
 * */
 
 public class Student {
     int timeCommitment = 0;
-    ArrayList<int[]> classes;
-    String[] preferrence;
+    ArrayList<int[]> schedule;
+    String[] typeOfClub;
     int clubsize = 0;
 
     public void setTimeCommitment(int timeCommitment) {
         this.timeCommitment = timeCommitment;
     }
-
     public int getTimeCommitment() {
         return timeCommitment;
     }
-
-    public void setClasses(ArrayList<int[]> classes) {
-        this.classes = classes;
+    public void setSchedule(ArrayList<int[]> classes) {
+        this.schedule = classes;
     }
-
-    public ArrayList<int[]> getClasses() {
-        return classes;
+    public ArrayList<int[]> getSchedule() {
+        return schedule;
     }
-
     public void setClubsize(int clubsize) {
         this.clubsize = clubsize;
     }
-
     public int getClubsize() {
         return clubsize;
+    }
+    public boolean hasTimeCommitment(){
+        return timeCommitment != 0;
+    }
+    public boolean hasSchedule(){
+        return schedule != null;
+    }
+    public boolean hasTypeOfClub(){
+        return typeOfClub != null;
+    }
+    public boolean hasClubsize(){
+        return clubsize != 0;
+    }
+
+    public String[] getTypeOfClub() {
+        return typeOfClub;
+    }
+
+    public void setTypeOfClub(String[] typeOfClub) {
+        this.typeOfClub = typeOfClub;
     }
 }
