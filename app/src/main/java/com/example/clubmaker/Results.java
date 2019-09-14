5dc768edd192847ed5cdbeee7f03dd36f5d3fa76
Xiaohua Zhang <sparjly.skarles@gmail.com>
2019-09-14 15:28:53 -0400

    d

--- app/src/main/java/com/example/clubmaker/Results.java
+++ app/src/main/java/com/example/clubmaker/Results.java
@@ -3,6 +3,7 @@ package com.example.clubmaker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,5 +43,8 @@ public class Results extends AppCompatActivity {
         }catch(Error e){
             choice1.setText(e.getMessage());
         }
+    }
+    public void enterSurvey(View v) {
+        startActivity(new Intent(Results.this, Survey.class));
     }
 }
